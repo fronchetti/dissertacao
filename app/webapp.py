5e5b77bec70d86440b4ed2050562b7b69fbc34b6
Felipe Fronchetti <fronchettl@vcu.edu>
2022-12-15 15:53:20 -0500

    Upload changes from app

--- app/webapp.py
+++ app/webapp.py
@@ -3,23 +3,70 @@
 
 import streamlit as page
 from about_section import write_about_section
+from motivation_section import write_motivation_section
 from analysis_section import write_contributing_analysis
 
 page.set_page_config(
-     page_title="Analysis of Contributing Files",
-     page_icon="📋",
+     page_title="CONTRIBUTING.info",
+     page_icon="🔸",
      layout="centered",
      initial_sidebar_state="collapsed",
  )
 
-page.markdown("### contributing.info")
+page.markdown(
+    """
+    <style>
+    .custom-page-header {
+        font-size: 30px !important;
+        font-weight: bold;
+    }
 
-repository_url = page.text_input("What GitHub repository would you like to\
+    .custom-page-title {
+        font-size: 22px !important;
+        font-weight: bold;
+    }
+
+    .custom-container { 
+        padding: 15px;
+        border-radius: 5px;
+        background-color: #ebebeb;
+    }
+
+    .custom-percentage { 
+        padding: 15px;
+        text-align: center;
+        border-radius: 5px;
+        background-color: #ebebeb;
+    }
+
+    a { 
+        color: #47809e !important;
+        text-decoration: none;
+    }
+
+    * {
+        font-size: 20px !important;
+    }
+    </style>
+    """,
+    unsafe_allow_html=True,
+)
+
+page.markdown('<p class="custom-page-header">CONTRIBUTING.info</p>', unsafe_allow_html=True)
+
+about, motivation, classifier = page.tabs(["About", "Motivation", "Classifier"])
+
+with about:
+    write_about_section(page)
+
+with motivation:
+    write_motivation_section(page)
+
+with classifier:
+    repository_url = page.text_input("What GitHub repository would you like to\
                  analyze?", help="Please provide a valid URL to the repository\
                  hosted on GitHub that you would like to analyze.", 
                  placeholder="https://github.com/github/docs/", max_chars=2048)
 
-with page.spinner("Parsing documentation file..."):
-    write_contributing_analysis(page, repository_url)
-
-write_about_section(page)
+    with page.spinner("Parsing documentation file..."):
+        write_contributing_analysis(page, repository_url)
